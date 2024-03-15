103593b6937898f9ea71aea595c17dd577483c99
DexyMaroni <divineinyang7@mail.com>
2024-03-15 01:01:18 +0000

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,38 +1,29 @@
 #!/usr/bin/python3
+"""
+Determines if all boxes can be opened.
+
+"""
+
 
 def canUnlockAll(boxes):
     """
-    Determine if all boxes can be unlocked.
+    A method that determines if all the boxes can be opened.
 
-    Args:
-    - boxes (list of lists): A list of lists representing the boxes and their keys.
+    :Param:
+        boxes
 
-    Returns:
-    - bool: True if all boxes can be unlocked, False otherwise.
+    :Return:
+        True or False
     """
 
-    # Get the total number of boxes
-    n = len(boxes)
+    if not boxes or type(boxes) is not list:
+        return False
 
-    # Initialize a list to keep track of visited boxes
-    visited = [False] * n
-
-    # Start DFS from box 0
-    stack = [0]
-
-    # Depth-First Search (DFS) algorithm
-    while stack:
-        # Pop a box from the stack
-        box = stack.pop()
-        
-        # Mark the box as visited
-        visited[box] = True
-
-        # Iterate over the keys in the current box
-        for key in boxes[box]:
-            # If the key leads to an unvisited box, add it to the stack
-            if not visited[key]:
-                stack.append(key)
-
-    # Check if all boxes have been visited
-    return all(visited)
+    unlocked = [0]
+    for n in unlocked:
+        for key in boxes[n]:
+            if key not in unlocked and key < len(boxes):
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
