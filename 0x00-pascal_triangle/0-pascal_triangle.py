ba6465c40e6faeb8ada79a9b3e3d623bd8741357
DexyMaroni <divineinyang7@mail.com>
2024-04-04 14:58:27 +0000

    Pascal Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+# Define the function
+
+def pascalTriangle(num):
+
+"""Create a list to store all rows"""
+    triangle = []
+# Get a variable to store the number of rows.
+    for rows_num in range(num):
+
+        # Create a row to represent the rows
+        row = [None for _ in range(rows_num+1)]
+
+# Set the first and last values to 1
+        row[0], row[-1] = 1, 1
+# Find a way to apply the binomial formula
+        for j in range(1, len(row) - 1):
+            row[j] = triangle[rows_num - 1] [j - 1] + triangle[rows_num - 1] [j]
+
+        triangle.append(row)
+    return triangle
+
